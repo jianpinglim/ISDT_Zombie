95db2ffbb1093039c704c4ada327f688a606191d
Jianpinglim <jianpinglim62@gmail.com>
2025-02-16 20:30:23 +0800

    tracker is done

--- ISDT_CA1_Jianping/Assets/GrabHandPose.cs
+++ ISDT_CA1_Jianping/Assets/GrabHandPose.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
-
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 public class GrabHandPose : MonoBehaviour
 {
+    public HandData rightHandPose;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.selectEntered.AddListener(SetUpPose);
+        rightHandPose.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    public void SetUpPose(BaseInteractionEventArgs arg){
+        if(arg.interactorObject is XRDirectInteractor){
+            HandData handData = arg.interactorObject.transform.GetComponentInChildren<HandData>();
+            handData.animator.enabled = false;
+        }
     }
 }
